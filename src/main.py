ed6a0a1e6bd03b24763cf33b7fc6299c2d4ff92b
Nagico <nagico@qq.com>
2025-01-24 17:21:11 +0800

    fix: int type of worker num

--- src/main.py
+++ src/main.py
@@ -12,4 +12,4 @@ app.static('/statics', CONFIG.STATIC_DIR)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=CONFIG.DEBUG, workers=os.environ.get("WORKERS", 1))
+    app.run(host="0.0.0.0", port=8000, debug=CONFIG.DEBUG, workers=int(os.environ.get("WORKERS", "1")))
